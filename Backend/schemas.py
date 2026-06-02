53ec34340c1b6e40bae3259c1f52d77a3d42927b
WDS714 <126628934+WDS714@users.noreply.github.com>
2026-06-02 04:02:28 +0000

    Agregar formulario de citas con calendario, validaciones y fondo animado

--- Backend/schemas.py
+++ Backend/schemas.py
@@ -4,3 +4,15 @@ class ProductoSchema(BaseModel):
     nombre: str
     precio: float
     disponible: bool
+
+class CitaSchema(BaseModel):
+    nombre: str
+    apellido: str
+    cedula: str
+    fecha_nacimiento: str
+    correo: str
+    telefono: str
+    servicio: str
+    fecha: str
+    hora: str
+    comentario: str = None
